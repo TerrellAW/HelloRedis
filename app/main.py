1c8499050d2430ab7520dbc0f9e3fda8734d13a3
TerrellAW <terraw1999@proton.me>
2026-01-30 15:46:37 -0700

    Add: Redis connection

--- app/main.py
+++ app/main.py
@@ -2,9 +2,14 @@ from flask import Flask
 import redis
 
 app = Flask(__name__)
+r = redis.Redis(host='localhost', port=6379, decode_responses=True)
 
 @app.route('/')
 def hello():
+    name = r.get('name')
+    if name:
+        return f"Hello {name}"
+
     return "Hello Guest"
 
 if __name__ == '__main__':
